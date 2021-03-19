77023447ed26b5b3aaba17d87a1b43c1d8da4e5b
lele <562282501@qq.com>
2021-03-19 14:45:02 +0800

    处理Axios在不同环境下的基础地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
-const service = axios.create()
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // 环境变量
+  timeout: 5000 // 超时时间
+})
 service.interceptors.request.use()
 service.interceptors.response.use()
 export default service
