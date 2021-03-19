41b7983065bdabcba4096099bd827cc6a34cd948
lele <562282501@qq.com>
2021-03-19 11:24:00 +0800

    Vuex中操作token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,23 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+const state = {
+  token: getToken()
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    // 将token存储到缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {}
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
