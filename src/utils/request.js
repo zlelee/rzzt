a3df6b737cfdef9e4016839fb039ec8053efd4f5
lele <562282501@qq.com>
2021-03-19 11:18:37 +0800

    封装单独的登录接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
-const service = axios.create(
-)
+const service = axios.create()
 service.interceptors.request.use()
 service.interceptors.response.use()
-
 export default service
