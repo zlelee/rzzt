55b189fbe9c3ff4b10860fe58a47ea861f08e4a4
lele <562282501@qq.com>
2021-03-20 14:35:21 +0800

    完成自定义指令的全局注册

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -14,7 +14,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="staffPhoto" class="user-avatar">
+          <img v-imagerror="defaultImg" :src="staffPhoto" class="user-avatar">
           <span class="name">{{ name }}</span>
           <i class="el-icon-caret-bottom" />
         </div>
@@ -46,6 +46,7 @@ export default {
   },
   data() {
     return {
+      defaultImg: require('@/assets/common/head.jpg')
     }
   },
   computed: {
--- src/main.js
+++ src/main.js
@@ -14,6 +14,10 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import * as directives from '@/directives'
+Object.keys(directives).forEach(key => {
+  Vue.directive(key, directives[key])
+})
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
