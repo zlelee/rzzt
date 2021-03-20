05cbedde2a4043baff71db3fbc50229dc1bc47cf
lele <562282501@qq.com>
2021-03-20 11:15:52 +0800

    权限拦截器调用获取用户信息的Action

--- src/permission.js
+++ src/permission.js
@@ -2,15 +2,17 @@ import router from '@/router'
 import store from '@/store'
 import NProgress from 'nprogress' // 引入一份进度条插件
 import 'nprogress/nprogress.css' // 引入进度条样式
-
 const whiteList = ['/login', '/404']
-router.beforeEach((to, from, next) => {
+router.beforeEach(async(to, from, next) => {
   NProgress.start()
   if (store.getters.token) {
     // 登录了
     if (to.path === '/login') {
       next('/') // 跳到首页
     } else {
+      if (!store.state.user.userInfo.userId) {
+        await store.dispatch('user/getUserInfo')
+      }
       next() // 放行
     }
   } else {
--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,6 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   token: state => state.user.token, // 用户token
-  name: state => state.user.usesInfo.username
+  name: state => state.user.userInfo.username
 }
 export default getters
