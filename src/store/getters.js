fb69f44b4ee98e1d3660e9995ab6ea02aa3b21d7
lele <562282501@qq.com>
2021-03-20 10:28:28 +0800

    封装获取用户资料的Action并共享用户状态

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,7 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token: state => state.user.token
+  token: state => state.user.token, // 用户token
+  name: state => state.user.usesInfo.username
 }
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,9 @@
 import { login } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
+import { getUserInfo } from '@/api/user'
 const state = {
-  token: getToken()
+  token: getToken(),
+  userInfo: {} // 设置一个空对象而不是null, 因为后面要通过userinfo.username的方式取数据,避免了报错
 }
 const mutations = {
   setToken(state, token) {
@@ -13,12 +15,22 @@ const mutations = {
   removeToken(state) {
     state.token = null
     removeToken()
+  },
+  setUserInfo(state, userInfo) {
+    state.userInfo = { ...userInfo } // 用浅拷贝的方式去赋值对象, 因为这样数据更新之后,才会触发组件的更新
+  },
+  removeUserInfo(state) {
+    state.userInfo = {}
   }
 }
 const actions = {
   async login(context, data) {
     const result = await login(data)
     context.commit('setToken', result)
+  },
+  async getUserInfo(context) {
+    const result = await getUserInfo()
+    context.commit('setUserInfo', result)
   }
 }
 export default {
