40d914e8d9f9809207fb8fa0fbcd2194ecb99a27
lele <562282501@qq.com>
2021-03-21 14:32:05 +0800

    应用封装的树Item

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,9 +2,11 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card class="tree-card">
+        <tree-item :tree-node="company" :is-root="true" />
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
           <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
           <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
+          <tree-item slot-scope="{data}" :tree-node="data" />
         </el-tree>
       </el-card>
     </div>
