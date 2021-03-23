d27373b30b855caa1c509d4b4d3c976cd0b485b7
lele <562282501@qq.com>
2021-03-23 11:16:14 +0800

    完成新增表单的基本校验条件

--- src/views/departments/components/add-depts.vue
+++ src/views/departments/components/add-depts.vue
@@ -3,18 +3,18 @@
   <el-dialog title="新增部门" :visible="showDialog">
     <!-- 表单组件  el-form   label-width设置label的宽度   -->
     <!-- 匿名插槽 -->
-    <el-form label-width="120px">
-      <el-form-item label="部门名称">
-        <el-input style="width:80%" placeholder="1-50个字符" />
+    <el-form :model="formData" :rules="rules" label-width="120px">
+      <el-form-item label="部门名称" prop="name">
+        <el-input v-model="formData.name" style="width:80%" placeholder="1-50个字符" />
       </el-form-item>
-      <el-form-item label="部门编码">
-        <el-input style="width:80%" placeholder="1-50个字符" />
+      <el-form-item label="部门编码" prop="code">
+        <el-input v-model="formData.code" style="width:80%" placeholder="1-50个字符" />
       </el-form-item>
-      <el-form-item label="部门负责人">
-        <el-select style="width:80%" placeholder="请选择" />
+      <el-form-item label="部门负责人" prop="manager">
+        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择" />
       </el-form-item>
-      <el-form-item label="部门介绍">
-        <el-input style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
+      <el-form-item label="部门介绍" prop="introduce">
+        <el-input v-model="formData.introduce" style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
       </el-form-item>
     </el-form>
     <!-- el-dialog有专门放置底部操作栏的 插槽  具名插槽 -->
@@ -33,11 +33,28 @@ export default {
   props: {
     showDialog: {
       type: Boolean,
-      required: true
+      default: false
     }
   },
   data() {
     return {
+      // 定义表单数据
+      formData: {
+        name: '', // 部门名称
+        code: '', // 部门编码
+        manager: '', // 部门管理者
+        introduce: '' // 部门介绍
+      },
+      // 定义校验规则
+      rules: {
+        name: [{ required: true, message: '部门名称不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门名称要求1-50个字符', trigger: 'blur' }],
+        code: [{ required: true, message: '部门编码不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门编码要求1-50个字符', trigger: 'blur' }],
+        manager: [{ required: true, message: '部门负责人不能为空', trigger: 'blur' }],
+        introduce: [{ required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          { trigger: 'blur', min: 1, max: 300, message: '部门介绍要求1-50个字符' }]
+      }
     }
   },
 
