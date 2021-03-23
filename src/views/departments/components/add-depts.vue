49c837afbbd98a3bb894e37a9ade350a7ee8bd2b
lele <562282501@qq.com>
2021-03-23 11:19:01 +0800

    配置新增表单的基本校验规则

--- src/views/departments/components/add-depts.vue
+++ src/views/departments/components/add-depts.vue
@@ -5,16 +5,34 @@
     <!-- 匿名插槽 -->
     <el-form :model="formData" :rules="rules" label-width="120px">
       <el-form-item label="部门名称" prop="name">
-        <el-input v-model="formData.name" style="width:80%" placeholder="1-50个字符" />
+        <el-input
+          v-model="formData.name"
+          style="width:80%"
+          placeholder="1-50个字符"
+        />
       </el-form-item>
       <el-form-item label="部门编码" prop="code">
-        <el-input v-model="formData.code" style="width:80%" placeholder="1-50个字符" />
+        <el-input
+          v-model="formData.code"
+          style="width:80%"
+          placeholder="1-50个字符"
+        />
       </el-form-item>
       <el-form-item label="部门负责人" prop="manager">
-        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择" />
+        <el-select
+          v-model="formData.manager"
+          style="width:80%"
+          placeholder="请选择"
+        />
       </el-form-item>
       <el-form-item label="部门介绍" prop="introduce">
-        <el-input v-model="formData.introduce" style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
+        <el-input
+          v-model="formData.introduce"
+          style="width:80%"
+          placeholder="1-300个字符"
+          type="textarea"
+          :rows="3"
+        />
       </el-form-item>
     </el-form>
     <!-- el-dialog有专门放置底部操作栏的 插槽  具名插槽 -->
@@ -47,13 +65,36 @@ export default {
       },
       // 定义校验规则
       rules: {
-        name: [{ required: true, message: '部门名称不能为空', trigger: 'blur' },
-          { min: 1, max: 50, message: '部门名称要求1-50个字符', trigger: 'blur' }],
-        code: [{ required: true, message: '部门编码不能为空', trigger: 'blur' },
-          { min: 1, max: 50, message: '部门编码要求1-50个字符', trigger: 'blur' }],
-        manager: [{ required: true, message: '部门负责人不能为空', trigger: 'blur' }],
-        introduce: [{ required: true, message: '部门介绍不能为空', trigger: 'blur' },
-          { trigger: 'blur', min: 1, max: 300, message: '部门介绍要求1-50个字符' }]
+        name: [
+          { required: true, message: '部门名称不能为空', trigger: 'blur' },
+          {
+            min: 1,
+            max: 50,
+            message: '部门名称要求1-50个字符',
+            trigger: 'blur'
+          }
+        ],
+        code: [
+          { required: true, message: '部门编码不能为空', trigger: 'blur' },
+          {
+            min: 1,
+            max: 50,
+            message: '部门编码要求1-50个字符',
+            trigger: 'blur'
+          }
+        ],
+        manager: [
+          { required: true, message: '部门负责人不能为空', trigger: 'blur' }
+        ],
+        introduce: [
+          { required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          {
+            trigger: 'blur',
+            min: 1,
+            max: 300,
+            message: '部门介绍要求1-50个字符'
+          }
+        ]
       }
     }
   },
@@ -62,6 +103,4 @@ export default {
 }
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang="less" scoped></style>
