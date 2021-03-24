277c9b53da6fba60f699d5ae7596c8eefbe9a494
lele <562282501@qq.com>
2021-03-24 14:06:42 +0800

    删除角色功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -19,9 +19,11 @@
               <el-table-column align="center" prop="name" label="角色名称" width="240" />
               <el-table-column align="center" prop="description" label="描述" />
               <el-table-column align="center" label="操作">
-                <el-button size="small" type="success">分配权限</el-button>
-                <el-button size="small" type="primary">编辑</el-button>
-                <el-button size="small" type="danger">删除</el-button>
+                <template slot-scope="{row}">
+                  <el-button size="small" type="success">分配权限</el-button>
+                  <el-button size="small" type="primary">编辑</el-button>
+                  <el-button size="small" type="danger" @click="delRole(row)">删除</el-button>
+                </template>
               </el-table-column>
             </el-table>
             <!-- 分页组件 -->
@@ -65,8 +67,9 @@
 </template>
 
 <script>
-import { getRoleList, getCompanyInfo } from '@/api/setting'
+import { getRoleList, getCompanyInfo, deleteRole } from '@/api/setting'
 export default {
+  name: 'Setting',
   data() {
     return {
       roleList: [], // 角色列表
@@ -101,6 +104,17 @@ export default {
     },
     async getCompanyInfo() {
       this.formData = await getCompanyInfo(this.$store.state.user.userInfo.companyId)
+    },
+    async delRole(row) {
+      try {
+        await this.$confirm('确认删除该角色吗')
+        // 只有点击了确定 才能进入到下方
+        await deleteRole(row.id) // 调用删除接口
+        this.getRoleList() // 重新加载数据
+        this.$message.success('删除角色成功')
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 
