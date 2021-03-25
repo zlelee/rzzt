7e36de0ef76ed7bab4333078684993fbd4650967
lele <562282501@qq.com>
2021-03-25 14:32:32 +0800

    员工导入组件封装

---
+++ src/components/UploadExcel/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="upload-excel">
+    <div class="btn-upload">
+      <el-button
+        :loading="loading"
+        size="mini"
+        type="primary"
+        @click="handleUpload"
+      >
+        点击上传
+      </el-button>
+    </div>
+
+    <input
+      ref="excel-upload-input"
+      class="excel-upload-input"
+      type="file"
+      accept=".xlsx, .xls"
+      @change="handleClick"
+    >
+    <div
+      class="drop"
+      @drop="handleDrop"
+      @dragover="handleDragover"
+      @dragenter="handleDragover"
+    >
+      <i class="el-icon-upload" />
+      <span>将文件拖到此处</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'UploadExcel'
+}
+</script>
+<style scoped lang="scss">
+.upload-excel {
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+  .excel-upload-input {
+    display: none;
+    z-index: -9999;
+  }
+  .btn-upload,
+  .drop {
+    border: 1px dashed #bbb;
+    width: 350px;
+    height: 160px;
+    text-align: center;
+    line-height: 160px;
+  }
+  .drop {
+    line-height: 80px;
+    color: #bbb;
+    i {
+      font-size: 60px;
+      display: block;
+    }
+  }
+}
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <page-tools :show-before="true">
-        <span slot="before">共166条记录</span>
-        <template slot="after">
-          <el-button size="small" type="warning">导入</el-button>
-          <el-button size="small" type="danger">导出</el-button>
-          <el-button size="small" type="primary">新增员工</el-button>
-        </template>
-      </page-tools>
+      <upload-excel />
     </div>
   </div>
 </template>
