a72039c785a23a73e085cc76a552f8d65f6fe29b
lele <562282501@qq.com>
2021-03-25 14:22:40 +0800

    点击当前部门填充到表单及渲染聘用形式

--- src/views/employees/components/add-employees.vue
+++ src/views/employees/components/add-employees.vue
@@ -12,7 +12,10 @@
         <el-date-picker v-model="formData.timeOfEntry" style="width:50%" placeholder="请选择日期" />
       </el-form-item>
       <el-form-item label="聘用形式" prop="formOfEmployment">
-        <el-select v-model="formData.formOfEmployment" style="width:50%" placeholder="请选择" />
+        <el-select v-model="formData.formOfEmployment" style="width:50%" placeholder="请选择">
+          <!-- 遍历只能遍历组件的数据 -->
+          <el-option v-for="item in EmployeeEnum.hireType" :key="item.id" :label="item.value" :value="item.id" />
+        </el-select>
       </el-form-item>
       <el-form-item label="工号" prop="workNumber">
         <el-input v-model="formData.workNumber" style="width:50%" placeholder="请输入工号" />
