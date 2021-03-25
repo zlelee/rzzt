95fcaf6ea58abb86ccbba76385447df0f79cfcf3
lele <562282501@qq.com>
2021-03-25 15:56:43 +0800

    实现导入功能

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-// import { importEmployee } from '@/api/employees'
+import { importEmployee } from '@/api/employees'
 export default {
   name: '',
 
@@ -16,8 +16,27 @@ export default {
   },
 
   methods: {
-    success({ header, results }) {
-
+    async success({ results }) {
+      // 如果是导入员工
+      const userRelations = {
+        '入职日期': 'timeOfEntry',
+        '手机号': 'mobile',
+        '姓名': 'username',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber'
+      }
+      console.log(results)
+      const arr = results.map(user => {
+        const userInfo = {}
+        console.log(Object.keys(user))
+        Object.keys(user).forEach(key => {
+          userInfo[userRelations[key]] = user[key]
+        })
+        return userInfo
+      })
+      await importEmployee(arr)
+      this.$message.success('导入excel成功')
+      this.$router.back()
     }
   }
 }
