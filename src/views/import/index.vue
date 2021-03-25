f7d4ab379a416e6674065b75b3cae2a74f4a7d4b
lele <562282501@qq.com>
2021-03-25 15:21:05 +0800

    建立公共导入的页面路由

---
+++ src/views/import/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- 公共导入组件 -->
+    <upload-excel />
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
