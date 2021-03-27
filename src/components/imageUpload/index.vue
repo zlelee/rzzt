3e0c0745e6ea990b403b7003f5b874b3d3f7f85a
lele <562282501@qq.com>
2021-03-27 18:40:13 +0800

    上传文件到腾讯云

--- src/components/imageUpload/index.vue
+++ src/components/imageUpload/index.vue
@@ -6,6 +6,7 @@
       :on-change="changeFile"
       :file-list="fileList"
       :before-upload="beforeUpload"
+      :http-request="upload"
       list-type="picture-card"
       action="#"
       :limit="1"
@@ -20,6 +21,11 @@
 </template>
 
 <script>
+import COS from 'cos-js-sdk-v5'
+var cos = new COS({
+  SecretId: 'AKIDEDvTmoa3jFXZKpmSaU7TYRKwHL8fqvys',
+  SecretKey: 'nKZXAYfa6QTFk00jFcjZ8QSK7dfRxC9q'
+})
 export default {
   name: '',
 
@@ -49,7 +55,7 @@ export default {
       this.fileList = fileList
     },
     changeFile(file, fileList) {
-      this.fileList.push({ url: file.url })
+      this.fileList.push({ url: file.url, status: 'success' })
     },
     beforeUpload(file) {
       const types = ['image/jpeg', 'image/gif', 'image/bmp', 'image/png']
@@ -63,7 +69,43 @@ export default {
         this.$message.error('图片大小最大不能超过5M')
         return false
       }
+
       return true
+    },
+    upload(params) {
+      if (params.file) {
+        console.log(params)
+        cos.putObject({
+          Bucket: 'hhh-1305395754', // 储存桶名称
+          Region: 'ap-beijing', // 地域
+          Key: params.file.uid.toString(), // 唯一标识
+          Body: params.file, // 上传文件对象
+          StorageClass: 'STANDARD' // 上传的模式类型 直接默认 标准模式即可
+          // 上传到腾讯云 =》 哪个存储桶 哪个地域的存储桶 文件  格式  名称 回调
+        }, (err, data) => {
+          // data返回数据之后 应该如何处理
+          // console.log(err || data)
+          // data中有一个statusCode === 200 的时候说明上传成功
+          if (!err && data.statusCode === 200) {
+            //   此时说明文件上传成功  要获取成功的返回地址
+            // fileList才能显示到上传组件上 此时我们要将fileList中的数据的url地址变成 现在上传成功的地址
+            // 目前虽然是一张图片 但是请注意 我们的fileList是一个数组
+            // 需要知道当前上传成功的是哪一张图片
+            this.fileList = this.fileList.map(item => {
+              // 去找谁的uid等于刚刚记录下来的id
+              if (item.uid === this.currentFileUid) {
+                // 将成功的地址赋值给原来的url属性
+                return { url: 'http://' + data.Location, upload: true }
+                // upload 为true 表示这张图片已经上传完毕 这个属性要为我们后期应用的时候做标记
+                // 保存  => 图片有大有小 => 上传速度有快又慢 =>要根据有没有upload这个标记来决定是否去保存
+              }
+              return item
+            })
+          }
+          // 将上传成功的地址 回写到了fileList中 fileList变化  =》 upload组件 就会根据fileList的变化而去渲染视图
+        }
+        )
+      }
     }
   }
 }
