47d4beaf9afafab1741de2c20371cc4acdaf0ed5
lele <562282501@qq.com>
2021-03-27 11:26:59 +0800

    实现用户名和密码的修改

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -4,14 +4,23 @@
       <el-card>
         <el-tabs>
           <el-tab-pane label="登录账户设置">
-
             <!-- 放置表单 -->
-            <el-form ref="userForm" :rules="rules" :model="userInfo" label-width="120px" style="margin-left: 120px; margin-top:30px">
+            <el-form
+              ref="userForm"
+              :rules="rules"
+              :model="userInfo"
+              label-width="120px"
+              style="margin-left: 120px; margin-top:30px"
+            >
               <el-form-item label="姓名:" prop="username">
                 <el-input v-model="userInfo.username" style="width:300px" />
               </el-form-item>
               <el-form-item label="密码:" prop="password">
-                <el-input v-model="userInfo.password2" style="width:300px" type="password" />
+                <el-input
+                  v-model="userInfo.password2"
+                  style="width:300px"
+                  type="password"
+                />
               </el-form-item>
               <el-form-item>
                 <el-button type="primary" @click="saveUser">更新</el-button>
@@ -35,9 +44,13 @@ export default {
   data() {
     return {
       rules: {
-        username: [{ required: true, message: '姓名不能为空', trigger: 'blur' }],
-        password2: [{ required: true, message: '密码不能为空', trigger: 'blur' },
-          { min: 6, max: 9, message: '密码长度6-9位', trigger: 'blur' }]
+        username: [
+          { required: true, message: '姓名不能为空', trigger: 'blur' }
+        ],
+        password2: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 9, message: '密码长度6-9位', trigger: 'blur' }
+        ]
       },
       userInfo: {
         username: '',
@@ -57,7 +70,10 @@ export default {
       try {
         // 校验
         await this.$refs.userForm.validate()
-        await saveUserDetailById({ ...this.userInfo, password: this.userInfo.password2 }) // 将新密码的值替换原密码的值
+        await saveUserDetailById({
+          ...this.userInfo,
+          password: this.userInfo.password2
+        }) // 将新密码的值替换原密码的值
         this.$message.success('保存成功')
       } catch (error) {
         console.log(error)
@@ -67,6 +83,4 @@ export default {
 }
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang="less" scoped></style>
