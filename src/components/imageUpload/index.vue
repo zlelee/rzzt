be84a6aef46619c0bb8fb15bfe45430319c952cc
lele <562282501@qq.com>
2021-03-27 17:55:27 +0800

    点击图片进行预览

--- src/components/imageUpload/index.vue
+++ src/components/imageUpload/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <el-upload list-type="picture-card" :limit="1" action="#" :on-preview="preview">
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <el-upload
+      :on-preview="preview"
+      :on-remove="handleRemove"
+      :on-change="changeFile"
+      :file-list="fileList"
+      list-type="picture-card"
+      action="#"
+      :limit="1"
+      :class="{disabled: fileComputed }"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog title="图片" :visible.sync="showDialog">
+      <img :src="imgUrl" style="width:100%" alt="">
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -10,22 +24,38 @@ export default {
 
   data() {
     return {
-      fileList: [], // 图片地址设置为数组
+      fileList: [{ url: 'https://img2.baidu.com/it/u=3355464299,584008140&fm=26&fmt=auto&gp=0.jpg' }], // 图片地址设置为数组
       showDialog: false, // 控制显示弹层
       imgUrl: ''
     }
   },
-
+  computed: {
+    // 设定一个计算属性 判断是否已经上传完了一张
+    fileComputed() {
+      return this.fileList.length === 1
+    }
+  },
   methods: {
     preview(file) {
       // 这里应该弹出一个层 层里是点击的图片地址
       this.imgUrl = file.url
       this.showDialog = true
+    },
+    handleRemove(file, fileList) {
+      // file是点击删除的文件
+    //   将原来的文件给排除掉了 剩下的就是最新的数组了
+      // this.fileList.filter(item => item.uid !== file.uid)
+      this.fileList = fileList
+    },
+    changeFile(file, fileList) {
+      this.fileList.push({ url: file.url })
     }
   }
 }
 </script>
 
-<style lang='less' scoped>
-
+<style scoped>
+.disabled >>> .el-upload--picture-card {
+  display: none
+}
 </style>
