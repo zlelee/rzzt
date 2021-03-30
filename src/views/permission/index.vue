61059e80b3e8f23712dc306f975982e6df609662
lele <562282501@qq.com>
2021-03-30 10:27:17 +0800

    添加权限点页面和对应接口

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,47 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <!-- 靠右的按钮 -->
+      <page-tools>
+        <template v-slot:after>
+          <el-button type="primary" size="small">添加权限</el-button>
+        </template>
+      </page-tools>
+      <!-- 表格 -->
+      <el-table :data="list" border row-key="id">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column label="标识" prop="code" />
+        <el-table-column label="描述" prop="description" />
+        <el-table-column align="center" label="操作">
+          <template>
+            <el-button type="text">添加</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+
+      </el-table>
     </div>
   </div>
 </template>
-
 <script>
+import { getPermissionList } from '@/api/permission'
+import { tranListToTreeData } from '@/utils'
 export default {
-
+  name: 'Permission',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermission()
+  },
+  methods: {
+    async getPermission() {
+      this.list = tranListToTreeData(await getPermissionList(), '0')
+    }
+  }
 }
 </script>
 
