b4ee184c3663b78636b92b80f9f65fe706dd2ea8
lele <562282501@qq.com>
2021-03-30 11:37:05 +0800

    准备编辑权限弹层

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -14,13 +14,41 @@
         <el-table-column label="描述" prop="description" />
         <el-table-column align="center" label="操作">
           <template slot-scope="{row}">
-            <el-button v-if="row.type===1" type="text">添加</el-button>
+            <el-button v-if="row.type===1" type="text" click="addPermission">添加</el-button>
             <el-button type="text">编辑</el-button>
             <el-button type="text">删除</el-button>
           </template>
         </el-table-column>
 
       </el-table>
+      <!-- 放置一个弹层 用来编辑新增节点 -->
+      <el-dialog title="新增编辑" :visible="showDialog">
+        <!-- 表单 -->
+        <el-form ref="perForm" :model="formData" :rules="rules" label-width="120px">
+          <el-form-item label="权限名称" prop="name">
+            <el-input v-model="formData.name" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限标识" prop="code">
+            <el-input v-model="formData.code" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限描述">
+            <el-input v-model="formData.description" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="开启">
+            <el-switch
+              v-model="formData.enVisible"
+              active-value="1"
+              inactive-value="0"
+            />
+          </el-form-item>
+        </el-form>
+        <el-row slot="footer" type="flex" justify="center">
+          <el-col :span="6">
+            <el-button size="small" type="primary">确定</el-button>
+            <el-button size="small">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -31,7 +59,20 @@ export default {
   name: 'Permission',
   data() {
     return {
-      list: []
+      list: [],
+      formData: {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      },
+      rules: {
+        name: [{ required: true, message: '权限名称不能为空', trigger: 'blur' }],
+        code: [{ required: true, message: '权限标识不能为空', trigger: 'blur' }]
+      },
+      showDialog: false
     }
   },
   created() {
@@ -40,6 +81,10 @@ export default {
   methods: {
     async getPermission() {
       this.list = tranListToTreeData(await getPermissionList(), '0')
+    },
+    addPermission() {
+      console.log(11)
+      this.showDialog = true
     }
   }
 }
