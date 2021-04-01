03955ec4e884f6551757dfd4252a4be0f52c0cdf
lele <562282501@qq.com>
2021-04-01 14:39:50 +0800

    工作日历组件结构

--- src/views/dashboard/components/work-calendar.vue
+++ src/views/dashboard/components/work-calendar.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
     <el-row type="flex" justify="end">
-      <el-select v-model="currentYear" size="small" style="width: 120px" @change="dateChange">
-        <el-option v-for="item in yearList" :key="item" :label="item" :value="item">{{ item }}</el-option>
+      <el-select
+        v-model="currentYear"
+        size="small"
+        style="width: 120px"
+        @change="dateChange"
+      >
+        <el-option
+          v-for="item in yearList"
+          :key="item"
+          :label="item"
+          :value="item"
+        >{{ item }}</el-option>
       </el-select>
-      <el-select v-model="currentMonth" size="small" style="width: 120px;margin-left:10px" @change="dateChange">
-        <el-option v-for="item in 12" :key="item" :label="item" :value="item">{{ item }}</el-option>
+      <el-select
+        v-model="currentMonth"
+        size="small"
+        style="width: 120px;margin-left:10px"
+        @change="dateChange"
+      >
+        <el-option v-for="item in 12" :key="item" :label="item" :value="item">{{
+          item
+        }}</el-option>
       </el-select>
     </el-row>
     <el-calendar v-model="currentDate">
       <template v-slot:dateCell="{ date, data }" class="content">
         <div class="date-content">
-          <span class="text"> {{ data.day | getDay }}</span>
-          <span v-if="isWeek(date)" class="rest">休</span>
+          <span class="text"> {{ data.day }}</span>
+          <span class="rest">休</span>
         </div>
       </template>
     </el-calendar>
@@ -21,6 +38,8 @@
 
 <script>
 export default {
+  filters: {
+  },
   props: {
     startDate: {
       type: Date,
@@ -34,47 +53,23 @@ export default {
       currentDate: null,
       yearList: []
     }
+  },
+  created() {
+    this.currentYear = new Date().getFullYear()
+    this.currentMonth = new Date().getMonth() + 1
+    this.yearList = Array.from(new Array(11), (value, index) => index + this.currentYear - 5)
+    this.dateChange()
+  },
+  methods: {
+    dateChange() {
+      const year = this.currentYear
+      const month = this.currentMonth
+      this.currentDate = new Date(`${year}-${month}-1`)
+    }
   }
 }
 </script>
 
-<style  scoped>
- /deep/ .el-calendar-day {
-  height:  auto;
- }
- /deep/ .el-calendar-table__row td,/deep/ .el-calendar-table tr td:first-child,  /deep/ .el-calendar-table__row td.prev{
-  border:none;
- }
-.date-content {
-  height: 40px;
-  text-align: center;
-  line-height: 40px;
-  font-size: 14px;
-}
-.date-content .rest {
-  color: #fff;
-  border-radius: 50%;
-  background: rgb(250, 124, 77);
-  width: 20px;
-  height: 20px;
-  line-height: 20px;
-  display: inline-block;
-  font-size: 12px;
-  margin-left: 10px;
-}
-.date-content .text{
-  width: 20px;
-  height: 20px;
-  line-height: 20px;
- display: inline-block;
+<style scoped>
 
-}
- /deep/ .el-calendar-table td.is-selected .text{
-   background: #409eff;
-   color: #fff;
-   border-radius: 50%;
- }
- /deep/ .el-calendar__header {
-   display: none
- }
 </style>
