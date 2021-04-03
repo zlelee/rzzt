d74c6a5f2ae6c8924fa38b806229a022873f6568
lele <562282501@qq.com>
2021-04-03 11:35:29 +0800

    引入自定义语言包

--- src/lang/index.js
+++ src/lang/index.js
@@ -3,16 +3,20 @@ import VueI18n from 'vue-i18n'
 import cookie from 'js-cookie'
 import elementEN from 'element-ui/lib/locale/lang/en'
 import elementZH from 'element-ui/lib/locale/lang/zh-CN'
+import customZH from './zh'
+import customEN from './en'
 Vue.use(VueI18n)
 
 export default new VueI18n({
   locale: cookie.get('language') || 'zh',
   messages: {
     en: {
-      ...elementEN
+      ...elementEN,
+      ...customEN
     },
     zh: {
-      ...elementZH
+      ...elementZH,
+      ...customZH
     }
   }
 })
